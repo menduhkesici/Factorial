4c57e6554f9b74f9c031249edd6f71de9791c443
Menduh Kesici <46794167+menduhkesici@users.noreply.github.com>
2022-10-28 16:05:40 +0200

    Update bench.cpp

--- bench.cpp
+++ bench.cpp
@@ -3,58 +3,44 @@
 
 #include "include/factorial.hpp"
 
-volatile std::string *_sink;
-
 static void BM_factorial1000(benchmark::State &state) {
   for (auto _ : state) {
-    auto b = factorial(1000);
-    asm volatile("" ::: "memory");
-    _sink = &b;
+    auto result = factorial(1000);
   }
 }
 BENCHMARK(BM_factorial1000);
 
 static void BM_factorial5000(benchmark::State &state) {
   for (auto _ : state) {
-    auto b = factorial(5000);
-    asm volatile("" ::: "memory");
-    _sink = &b;
+    auto result = factorial(5000);
   }
 }
 BENCHMARK(BM_factorial5000);
 
 static void BM_factorial10000(benchmark::State &state) {
   for (auto _ : state) {
-    auto b = factorial(10000);
-    asm volatile("" ::: "memory");
-    _sink = &b;
+    auto result = factorial(10000);
   }
 }
 BENCHMARK(BM_factorial10000);
 
 static void BM_factorial50000(benchmark::State &state) {
   for (auto _ : state) {
-    auto b = factorial(50000);
-    asm volatile("" ::: "memory");
-    _sink = &b;
+    auto result = factorial(50000);
   }
 }
 BENCHMARK(BM_factorial50000);
 
 static void BM_factorial80000(benchmark::State &state) {
   for (auto _ : state) {
-    auto b = factorial(80000);
-    asm volatile("" ::: "memory");
-    _sink = &b;
+    auto result = factorial(80000);
   }
 }
 BENCHMARK(BM_factorial80000);
 
 static void BM_factorial100000(benchmark::State &state) {
   for (auto _ : state) {
-    auto b = factorial(100000);
-    asm volatile("" ::: "memory");
-    _sink = &b;
+    auto result = factorial(100000);
   }
 }
 BENCHMARK(BM_factorial100000);
